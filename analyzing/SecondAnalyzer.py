845bf17200828ef90b21e5217e4bb15626c7fb4a
Jochem Gelten <jochem.gelten@gmail.com>
2022-12-23 11:16:43 +0100

    It now allows setting custom bounds for the Q scores in implementation 2

--- analyzing/SecondAnalyzer.py
+++ analyzing/SecondAnalyzer.py
@@ -5,8 +5,9 @@ import copy
 
 class SecondAnalyzer(Analyzer):
 
-    def __init__(self, sampler_strategy, perturber_strategy, sample_size=10000):
+    def __init__(self, sampler_strategy, perturber_strategy, sample_size=10000, bounds = [25, 30]):
         super().__init__(sampler_strategy, perturber_strategy, sample_size)
+        self.BOUNDS = bounds
 
     def print_analyze(self):
         print("Analyzing using SecondAnalyzer")
@@ -17,8 +18,8 @@ class SecondAnalyzer(Analyzer):
         print("Percentage of potential errors that were repaired or correct: ", analysis[3]) #rep_perc2
 
         print("Percentage of actual errors that were unrepairable and have Q score >= 25: ", analysis[4])
-        print("Percentage of incorrect uncertainties: ", analysis[5])
-        print("Percentage of correct readings with 25 <= Q score < 30: ", analysis[6])
+        print("Percentage of correct readings with ", self.BOUNDS[0], " <= Q score < ", self.BOUNDS[1], ": ", analysis[5])
+        print("Percentage of incorrect uncertainties: ", analysis[6])
         print("Percentage of readings fixed by guessing: ", analysis[7])
         print('\n')
 
@@ -37,12 +38,12 @@ class SecondAnalyzer(Analyzer):
 
         for j in range(self.SAMPLE_SIZE):
             seq = self.sampler.random_sequence()
-            pert = self.perturber.perturb_sequence(seq)
+            pert = self.perturber.perturb_sequence(seq, self.BOUNDS)
             attempted_fix = copy.deepcopy(pert[0])
 
             for i in range(len(pert[0])):
                 total += 1
-                if ord(pert[1][i]) < 63: # if the q_score_nr was lower than 30, so there could have been an error (defined in code)
+                if ord(pert[1][i]) < self.BOUNDS[1] + 33: # if the q_score_nr was lower than the upper bound, so there could have been an error (defined in code)
                     potential_errors += 1
                     if(pert[0][i] != seq[i]):
                         actual_errors += 1
@@ -51,7 +52,7 @@ class SecondAnalyzer(Analyzer):
                     
                     if (i % 3 == 2 and utils.is_ambig(pert[0][i-2:i+1])):
                         total_repairable += 1 # big overlap with this and correct_uncertainties  
-                    elif (ord(pert[1][i]) >=  58): # q_score >= 25 = 58 - 33 might be correct 
+                    elif (ord(pert[1][i]) >=  self.BOUNDS[0] + 33): # q_score_nr >= lower bound, so it might be correct
                         uncertain_unchanged += 1 
                         # need to change to fix acids not nucleotides
                         # if (utils.get_acid(attempted_fix[0][i-2:i+1]) == utils.get_acid(seq[i-2:i+1])):
@@ -72,27 +73,29 @@ class SecondAnalyzer(Analyzer):
                             unfixed_errors += 1
 
         potential_error_perc = (potential_errors / total) * 100
-        actual_error_perc = (actual_errors / total) * 100 # isn't printed
+        actual_error_perc = (actual_errors / total) * 100 
         rep_perc = ((potential_errors - total_repairable) / total) * 100 
-        rep_perc2 = (total_repairable / potential_errors) * 100 
+        rep_perc2 = ((total_repairable + correct_uncertainties) / potential_errors) * 100 
 
-        unrepairable_uncertain_perc = ((uncertain_unchanged - correct_uncertainties) / actual_errors) * 100 # fixed?
+        unrepairable_uncertain_perc = ((uncertain_unchanged - correct_uncertainties) / actual_errors) * 100 
         
         # return -1 if not possible (very low channce)
-        wrong_uncertainties = -1
         high_q_score_correct_perc = -1
+        unfixed_potential_errors = -1
         correct_guess_perc = -1
-
-        if (uncertain_unchanged > 0 or uncertain_changed > 0):
-            wrong_uncertainties = ((unfixed_errors) / (uncertain_unchanged + uncertain_changed)) * 100
 
         if (uncertain_unchanged > 0):
             # percentage of correct readings with 25 <= q_score < 30
             high_q_score_correct_perc = ((correct_uncertainties) / uncertain_unchanged) * 100 
+        
+        if (uncertain_unchanged > 0 or uncertain_changed > 0):
+            unfixed_potential_errors = ((unfixed_errors) / (uncertain_unchanged + uncertain_changed)) * 100
+
+        
         
         if (uncertain_changed > 0): 
             # percentage of readings that were correctly fixed by guessing, could be interesting for some models
             correct_guess_perc = ((uncertain_changed - (uncertain_changed - guessed_uncertainties)) / uncertain_changed) * 100 
            
 
-        return [potential_error_perc, actual_error_perc, rep_perc, rep_perc2, unrepairable_uncertain_perc, wrong_uncertainties, high_q_score_correct_perc, correct_guess_perc]
+        return [potential_error_perc, actual_error_perc, rep_perc, rep_perc2, unrepairable_uncertain_perc, high_q_score_correct_perc, unfixed_potential_errors, correct_guess_perc]
--- perturbing/QualityScorePerturber.py
+++ perturbing/QualityScorePerturber.py
@@ -7,7 +7,7 @@ import copy
 # Perturbs with a predefined error rate and generates a Q score dependent on a pass or fail.
 class QualityScorePerturber(Perturber):
 
-    def perturb_sequence(self, sequence):
+    def perturb_sequence(self, sequence, bounds):
         errors = []
         pert_seq = copy.deepcopy(sequence)
         for i in range(len(sequence)):
@@ -16,10 +16,10 @@ class QualityScorePerturber(Perturber):
                 temp = utils.NUCLEOTIDES.copy()
                 temp.remove(sequence[i])
                 pert_seq[i] = random.choice(temp)
-                q_score_nr = random.randrange(0, 30) # gives a q score < 30 for perturbed ones
+                q_score_nr = random.randrange(0, bounds[1]) # gives a q score < upper bound for perturbed ones
 
             else:
-                q_score_nr = random.randrange(25, 41) # gives a q score >= 25 for correct ones
+                q_score_nr = random.randrange(bounds[0], 41) # gives a q score >= lowe bound for correct ones
             
             q_score_ascii = chr(q_score_nr + 33)
             errors += [q_score_ascii] 
