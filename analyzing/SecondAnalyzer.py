7e859dbc37ac277baf77c6599ed7e78d99366f53
Jochem Gelten <jochem.gelten@gmail.com>
2023-01-13 20:34:21 +0100

    Added standard deviation calculations and outputs

--- analyzing/SecondAnalyzer.py
+++ analyzing/SecondAnalyzer.py
@@ -1,3 +1,4 @@
+import math
 from analyzing.Analyzer import Analyzer
 from utils import Utils as utils
 import random
@@ -18,11 +19,16 @@ class SecondAnalyzer(Analyzer):
         print("Percentage of potential errors that were repaired or correct: ", analysis[3]) #rep_perc2
         print("Actual error percentage after repairs: ", analysis[4]) #actual_rep_perc
         print("Percentage of actual errors that were repaired or correct: ", analysis[5]) #actual_rep_perc2
+        
+        print("Standard deviation for total potential errors: ", analysis[6])
+        print("Standard deviation for potential errors after repairs: ", analysis[7])
+        print("Standard deviation for total actual errors: ", analysis[8])
+        print("Standard deviation for actual errors after repairs: ", analysis[9])
 
-        print("Percentage of actual errors that were unrepairable and have Q score >=", self.BOUNDS[0], " : ", analysis[6])
-        print("Percentage of correct readings with ", self.BOUNDS[0], " <= Q score < ", self.BOUNDS[1], ": ", analysis[7])
-        print("Percentage of incorrect uncertainties: ", analysis[8])
-        print("Percentage of readings fixed by guessing: ", analysis[9])
+        print("Percentage of actual errors that were unrepairable and have Q score >=", self.BOUNDS[0], " : ", analysis[10])
+        print("Percentage of correct readings with ", self.BOUNDS[0], " <= Q score < ", self.BOUNDS[1], ": ", analysis[11])
+        print("Percentage of incorrect uncertainties: ", analysis[12])
+        print("Percentage of readings fixed by guessing: ", analysis[13])
         print('\n')
 
     def analyze(self):
@@ -38,10 +44,22 @@ class SecondAnalyzer(Analyzer):
         guessed_uncertainties = 0
         unfixed_errors = 0
 
+        potential_deviations = [0] * self.SAMPLE_SIZE
+        potential_repairable_deviations = [0] * self.SAMPLE_SIZE
+        actual_deviations = [0] * self.SAMPLE_SIZE
+        actual_repairable_deviations = [0] * self.SAMPLE_SIZE
+
         for j in range(self.SAMPLE_SIZE):
             seq = self.sampler.random_sequence()
             pert = self.perturber.perturb_sequence(seq, self.BOUNDS)
             attempted_fix = copy.deepcopy(pert[0])
+
+            old_total = total
+            old_potential_errors = potential_errors
+            old_total_repairable = total_repairable
+            old_correct_uncertainties = correct_uncertainties
+            old_actual_errors = actual_errors
+            old_actual_repairable = actual_errors_repairable
 
             for i in range(len(pert[0])):
                 total += 1
@@ -50,7 +68,7 @@ class SecondAnalyzer(Analyzer):
                     if(pert[0][i] != seq[i]):
                         actual_errors += 1
                         if (i % 3 == 2 and utils.is_ambig(pert[0][i-2:i+1])):
-                            actual_errors_repairable
+                            actual_errors_repairable += 1
                     
                     if (i % 3 == 2 and utils.is_ambig(pert[0][i-2:i+1])):
                         total_repairable += 1 # big overlap with this and correct_uncertainties  
@@ -74,12 +92,35 @@ class SecondAnalyzer(Analyzer):
                         else: 
                             unfixed_errors += 1
 
+            potential_deviations[j] = (potential_errors - old_potential_errors) / (total - old_total) * 100
+            potential_repairable_deviations[j] = total_repairable + correct_uncertainties - old_total_repairable - old_correct_uncertainties
+            potential_repairable_deviations[j] /= (total - old_total) * 100
+            actual_deviations[j] = (actual_errors - old_actual_errors) / (total - old_total) * 100
+            actual_repairable_deviations[j] = (actual_errors_repairable - old_actual_repairable) / (total - old_total) * 100
+
         potential_error_perc = (potential_errors / total) * 100
         actual_error_perc = (actual_errors / total) * 100 
         rep_perc = ((potential_errors - total_repairable) / total) * 100 
         actual_rep_perc = ((actual_errors - actual_errors_repairable) / total) * 100
         rep_perc2 = ((total_repairable + correct_uncertainties) / potential_errors) * 100 
         actual_rep_perc2 = ((actual_errors_repairable) / actual_errors) * 100 
+        print("actual_errors: ", actual_errors)
+        print("actual_errors_repairable: ", actual_errors_repairable)
+        # calculate sample standard deviation as we clearly only have a sample
+        potential_standard_deviation = 0
+        potential_standard_deviation_repairable = 0
+        actual_standard_deviation = 0
+        actual_standard_deviation_repairable = 0
+        for k in range(self.SAMPLE_SIZE):
+            potential_standard_deviation += math.pow(potential_deviations[k] - potential_error_perc, 2)
+            potential_standard_deviation_repairable += math.pow(potential_repairable_deviations[k] - rep_perc, 2)
+            actual_standard_deviation += math.pow(actual_deviations[k] - potential_error_perc, 2)
+            actual_standard_deviation_repairable += math.pow(actual_repairable_deviations[k] - actual_rep_perc, 2)
+
+        potential_standard_deviation = math.pow(potential_standard_deviation / (self.SAMPLE_SIZE - 1), 1/2)
+        potential_standard_deviation_repairable = math.pow(potential_standard_deviation_repairable / (self.SAMPLE_SIZE - 1), 1/2)
+        actual_standard_deviation = math.pow(actual_standard_deviation / (self.SAMPLE_SIZE - 1), 1/2)
+        actual_standard_deviation_repairable = math.pow(actual_standard_deviation_repairable / (self.SAMPLE_SIZE - 1), 1/2)
 
         unrepairable_uncertain_perc = ((uncertain_unchanged - correct_uncertainties) / actual_errors) * 100 
         
@@ -94,12 +135,12 @@ class SecondAnalyzer(Analyzer):
         
         if (uncertain_unchanged > 0 or uncertain_changed > 0):
             unfixed_potential_errors = ((unfixed_errors) / (uncertain_unchanged + uncertain_changed)) * 100
-
-        
         
         if (uncertain_changed > 0): 
             # percentage of readings that were correctly fixed by guessing, could be interesting for some models
-            correct_guess_perc = ((uncertain_changed - (uncertain_changed - guessed_uncertainties)) / uncertain_changed) * 100 
-           
+            correct_guess_perc = ((uncertain_changed - (uncertain_changed - guessed_uncertainties)) / uncertain_changed) * 100            
 
-        return [potential_error_perc, actual_error_perc, rep_perc, rep_perc2, actual_rep_perc, actual_rep_perc2, unrepairable_uncertain_perc, high_q_score_correct_perc, unfixed_potential_errors, correct_guess_perc]
+        return [potential_error_perc, actual_error_perc, rep_perc, rep_perc2, actual_rep_perc, actual_rep_perc2, 
+                    potential_standard_deviation, potential_standard_deviation_repairable, actual_standard_deviation, 
+                    actual_standard_deviation_repairable, unrepairable_uncertain_perc, high_q_score_correct_perc, 
+                    unfixed_potential_errors, correct_guess_perc]
--- analyzing/ThirdAnalyzer.py
+++ analyzing/ThirdAnalyzer.py
@@ -1,3 +1,4 @@
+import math
 from analyzing.Analyzer import Analyzer
 from utils import Utils as utils
 import random
@@ -14,8 +15,11 @@ class ThirdAnalyzer(Analyzer):
         print("Total error percentage: ", analysis[0])
         print("Error percentage after repairs: ", analysis[1])
         print("Percentage of errors repaired: ", analysis[2])
-        print("Minimal Q score to ensure error probability in all calls with this score or higher is less than the goal: ", analysis[3])
-        print("Percentage of readings with a Q score above 30 that are incorrect: ", analysis[4])
+        print("Standard deviation for total errors: ", analysis[3])
+        print("Standard deviation for errors after repairs: ", analysis[4])
+        print("Minimal Q score to ensure error probability in all calls with this score or higher is less than the goal: ", analysis[5])
+        print("Percentage of readings with a Q score above 30 that are incorrect: ", analysis[6])
+        
         print('\n')
 
     def analyze(self):
@@ -26,9 +30,16 @@ class ThirdAnalyzer(Analyzer):
         q_scores_error = [0] * 41
         q_scores_total = [0] * 41
 
+        deviations = [0] * self.SAMPLE_SIZE
+        repairable_deviations = [0] * self.SAMPLE_SIZE
+
         for j in range(self.SAMPLE_SIZE):
             seq = self.sampler.random_sequence()
             pert = self.perturber.perturb_sequence(seq)
+
+            old_total = total
+            old_total_repairable = total_repairable
+            old_total_errors = total_errors
 
             for i in range(len(pert[0])):
                 total += 1
@@ -40,10 +51,23 @@ class ThirdAnalyzer(Analyzer):
                     if i % 3 == 2:
                         if utils.is_ambig(pert[0][i-2:i+1]):
                             total_repairable += 1
+            
+            deviations[j] = (total_errors - old_total_errors) / (total - old_total) * 100
+            repairable_deviations[j] = (total_repairable - old_total_repairable) / (total - old_total) * 100
 
         error_perc = (total_errors / total) * 100
         rep_perc = ((total_errors - total_repairable) / total) * 100
         rep_perc2 = (total_repairable / total_errors) * 100
+
+        # calculate sample standard deviation as we clearly only have a sample
+        standard_deviation = 0
+        standard_deviation_repairable = 0
+        for k in range(self.SAMPLE_SIZE):
+            standard_deviation += math.pow(deviations[k] - error_perc, 2)
+            standard_deviation_repairable += math.pow(repairable_deviations[k] - rep_perc, 2)
+
+        standard_deviation = math.pow(standard_deviation / (self.SAMPLE_SIZE - 1), 1/2)
+        standard_deviation_repairable = math.pow(standard_deviation_repairable / (self.SAMPLE_SIZE - 1), 1/2)
 
         achieve_goal = 41 # we decrement it instantly so we start a bit higher
         q_score_error_count = q_scores_error[achieve_goal - 1]
@@ -54,9 +78,7 @@ class ThirdAnalyzer(Analyzer):
             q_score_error_count += q_scores_error[achieve_goal - 1] # add the values for the next highest Q score
             reading_count += q_scores_total[achieve_goal - 1]
             if(achieve_goal == 30):
-                geq_30_wrong_perc = q_score_error_count / reading_count
-
-            
+                geq_30_wrong_perc = q_score_error_count / reading_count            
             
         while (((q_score_error_count / reading_count) < self.ERROR_GOAL) and achieve_goal > 0):
             achieve_goal -= 1
@@ -65,4 +87,4 @@ class ThirdAnalyzer(Analyzer):
             if(achieve_goal == 30):
                 geq_30_wrong_perc = q_score_error_count / reading_count
 
-        return [error_perc, rep_perc, rep_perc2, achieve_goal, geq_30_wrong_perc]
+        return [error_perc, rep_perc, rep_perc2, standard_deviation, standard_deviation_repairable, achieve_goal, geq_30_wrong_perc]
