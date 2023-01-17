2a8e170df95ef0ff0f3b39a030f51bf69cce0566
Jochem Gelten <jochem.gelten@gmail.com>
2023-01-17 10:08:39 +0100

    added legend

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ analyzer3.print_analyze()
 start = 0
 end = 41
 x_axis = [j for j in range(start, end)]
-
+# The bounds for analizer 2
+boundsVar = [25, 30]
 # For analyzer 1
 print("analyzer 1 started")
 prob_standard_dev1 = np.zeros((2, 2, end - start))
@@ -42,7 +43,7 @@ for j in range(start, end):
 print("analyzer 2 started")
 prob_standard_dev2 = np.zeros((2, 2, end - start))
 for j in range(start, end):
-    analyzer2 = SecondAnalyzer(SamplerType.ACID_SAMPLING, PerturberType.QUALITY_SCORE_PERTURBING, sample_size=10000, fail_probability = math.pow(10,  -j / 10), bounds = [25, 30])
+    analyzer2 = SecondAnalyzer(SamplerType.ACID_SAMPLING, PerturberType.QUALITY_SCORE_PERTURBING, sample_size=10000, fail_probability = math.pow(10,  -j / 10), bounds = boundsVar)
     results_analyzer2 = analyzer2.analyze()
     i = j - start
     prob_standard_dev2[0][0][i] = results_analyzer2[0]
@@ -67,48 +68,55 @@ fig, axs = plt.subplots(3, sharex=True)
 
 # plot results analyzer 1 in first plot together with the standard deviation
 # normal error percentage:
-axs[0].plot(x_axis, prob_standard_dev1[0][0], 'red')
-axs[0].plot(x_axis, prob_standard_dev1[0][0] + prob_standard_dev1[0][1], 'blue')
+axs[0].plot(x_axis, prob_standard_dev1[0][0], 'red', label = "error %% without repairs")
+axs[0].plot(x_axis, prob_standard_dev1[0][0] + prob_standard_dev1[0][1], 'blue', label = "std dev without repairs")
 axs[0].plot(x_axis, prob_standard_dev1[0][0] - prob_standard_dev1[0][1], 'blue')
 # error percentage after repairs:
-axs[0].plot(x_axis, prob_standard_dev1[1][0], 'purple', linestyle = 'dashed')
-axs[0].plot(x_axis, prob_standard_dev1[1][0] + prob_standard_dev1[1][1], 'green', linestyle = 'dashed')
+axs[0].plot(x_axis, prob_standard_dev1[1][0], 'purple', linestyle = 'dashed', label = "error %% with repairs")
+axs[0].plot(x_axis, prob_standard_dev1[1][0] + prob_standard_dev1[1][1], 'green', linestyle = 'dashed', label = "std dev with repairs")
 axs[0].plot(x_axis, prob_standard_dev1[1][0] - prob_standard_dev1[1][1], 'green', linestyle = 'dashed')
 
 # plot results analyzer 2 in second plot together with the standard deviation
 # normal error percentage:
-axs[1].plot(x_axis, prob_standard_dev2[0][0], 'red')
-axs[1].plot(x_axis, prob_standard_dev2[0][0] + prob_standard_dev2[0][1], 'blue')
+axs[1].plot(x_axis, prob_standard_dev2[0][0], 'red', label = "error %% without repairs")
+axs[1].plot(x_axis, prob_standard_dev2[0][0] + prob_standard_dev2[0][1], 'blue', label = "std dev without repairs")
 axs[1].plot(x_axis, prob_standard_dev2[0][0] - prob_standard_dev2[0][1], 'blue')
 # error percentage after repairs:
-axs[1].plot(x_axis, prob_standard_dev2[1][0], 'purple', linestyle = 'dashed')
-axs[1].plot(x_axis, prob_standard_dev2[1][0] + prob_standard_dev2[1][1], 'green', linestyle = 'dashed')
+axs[1].plot(x_axis, prob_standard_dev2[1][0], 'purple', linestyle = 'dashed', label = "error %% with repairs")
+axs[1].plot(x_axis, prob_standard_dev2[1][0] + prob_standard_dev2[1][1], 'green', linestyle = 'dashed', label = "std dev with repairs")
 axs[1].plot(x_axis, prob_standard_dev2[1][0] - prob_standard_dev2[1][1], 'green', linestyle = 'dashed')
 
 # plot results analyzer 3 in second plot together with the standard deviation
 # minimal q score needed normally:
-axs[2].plot(x_axis, prob_standard_dev3[0][0], 'red')
-axs[2].plot(x_axis, prob_standard_dev3[0][0] + prob_standard_dev3[0][1], 'blue')
+axs[2].plot(x_axis, prob_standard_dev3[0][0], 'red', label = "min Q score needed without repairs")
+axs[2].plot(x_axis, prob_standard_dev3[0][0] + prob_standard_dev3[0][1], 'blue', label = "std dev without repairs")
 axs[2].plot(x_axis, prob_standard_dev3[0][0] - prob_standard_dev3[0][1], 'blue')
 # minimal q score needed after repairs:
-axs[2].plot(x_axis, prob_standard_dev3[1][0], 'purple', linestyle = 'dashed')
-axs[2].plot(x_axis, prob_standard_dev3[1][0] + prob_standard_dev3[1][1], 'green', linestyle = 'dashed')
+axs[2].plot(x_axis, prob_standard_dev3[1][0], 'purple', linestyle = 'dashed', label = "min Q score needed with repairs")
+axs[2].plot(x_axis, prob_standard_dev3[1][0] + prob_standard_dev3[1][1], 'green', linestyle = 'dashed', label = "std dev with repairs")
 axs[2].plot(x_axis, prob_standard_dev3[1][0] - prob_standard_dev3[1][1], 'green', linestyle = 'dashed')
 
-# Setting labels, comment these out to get plots with no text:
+# Setting labels and grids, comment these out to get plots with no text or grid:
 mplt.rcParams.update({'font.size': 10})
+mpl.rcParams['axes.formatter.useoffset'] = False
 fig.suptitle("Percentage of errors in the result as a function of the \n probability that a nucleotide in the sequence is perturbed.")
 axs[0].set(ylabel = "error percentage")
 axs[0].set_title("analyzer 1")
 # axs[0].set_title("Binary analyzer")
+axs[0].legend(loc = "upper right")
+axs[0].grid()
 
 axs[1].set(ylabel = "error percentage")
 axs[1].set_title("analyzer 2")
 # axs[1].set_title("Q score analyzer")
+axs[1].legend(loc = "upper right")
+axs[1].grid()
 
 axs[2].set(ylabel = "minimal Q score")
 axs[2].set_title("analyzer 3")
 # axs[2].set_title("Reverse Q score analyzer")
+axs[2].legend(loc = "upper left")
+axs[2].grid()
 
 # Set x axis label for lowest plot
 axs[2].set(xlabel = "Q score for error")
@@ -117,8 +125,6 @@ axs[2].set(xlabel = "Q score for error")
 for ax in axs:
     ax.label_outer()
 
-# Uncomment to remove the grid
-plt.grid()
 
 plt.show()
 
@@ -131,7 +137,7 @@ plt.show()
 #   I.e. what is the maximal error percentage (for analyzers 1 & 2) or minimal Q score (for analyzer 3) needed to have get 99% accuracy for repairable? 
 # + Can directly be answered from first graph with percentage of correct reads >= 99 I think
 
-# - is this an extra research question?
+# - is this an extra research question?mpl.rcParams['axes.formatter.useoffset'] = False
 # + SUGGESTION: if we have time:
 # Given that there is a mistake that we could not correct how many less mistakes should we make to fix the RNA sequence 
 # (including the repairable mistakes). Make a histogram for this per analyzer/error percentage
